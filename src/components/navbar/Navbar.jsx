230a67b9235356592f347efae694023f63df11c3
qriscilla <pri.whang@gmail.com>
2020-05-09 20:41:38 -0400

    Styled mobile navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,12 +7,12 @@ import {
   Toolbar,
   List,
   ListItem,
-  Divider,
   IconButton,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 import gcclogo from "../../static/images/logo/gcclogo.png";
+import logoBlack from "../../static/images/logo/gcclogo-black.png";
 import "./Navbar.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -71,7 +71,6 @@ function Navbar() {
                 <img
                   src={gcclogo}
                   alt={gcclogo}
-                  className="home__img"
                   style={{ width: "205.516px", height: "85px" }}
                 />
               </Link>
@@ -120,136 +119,48 @@ function Navbar() {
           </Grid>
 
           {/* Collapsed; Mobile */}
-          <IconButton
-            color="inherit"
-            onClick={handleDrawerOpen}
-            className={classes.mobile}
-          >
+          <IconButton color="inherit" onClick={handleDrawerOpen} className={classes.mobile}>
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
 
       <Drawer anchor="top" open={open}>
-        <div>
-          <IconButton onClick={handleDrawerClose}>
-            <MenuIcon />
-          </IconButton>
-        </div>
 
-        <List>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Welcome
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Beliefs
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Staff
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Mainline Site
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              AMI
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Family Groups
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Ministries
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Giving
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Multimedia
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/" style={collapse__links}>
-              Events
-            </Link>
-          </ListItem>
-        </List>
+        <Grid container style={{paddingTop:'30px',paddingLeft:'20px',paddingRight:'20px'}}>
+          <Grid item container xs={6}>
+            <Link to="/">
+              <img
+                src={logoBlack}
+                alt={logoBlack}
+                style={{ width: "205.516px", height: "85px" }}
+              />
+            </Link>            
+          </Grid>
+          <Grid item container xs={6} justify='flex-end'>
+            <IconButton onClick={handleDrawerClose}>
+              <MenuIcon />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            <List>
+              <ListItem><Link to="/" style={collapse__links}>Welcome</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Beliefs </Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Staff</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Mainline Site</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>AMI</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Family Groups</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Ministries</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Giving</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Multimedia</Link></ListItem>
+              <ListItem><Link to="/" style={collapse__links}>Events</Link></ListItem>
+            </List>               
+          </Grid>
+        </Grid>
 
-        <Divider />
       </Drawer>
     </div>
   );
 }
 
 export default Navbar;
-
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
-// import MenuIcon from "@material-ui/icons/Menu";
-// import gcclogo from "../../static/images/logo/gcclogo.png";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//     background: "transparent",
-//     boxShadow: "none",
-//     color: "white",
-//     position: "fixed",
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//   },
-// }));
-
-// export default function ButtonAppBar() {
-//   const classes = useStyles();
-
-//   return (
-//     <AppBar position="static" className={classes.root}>
-//       <Toolbar>
-//         <IconButton
-//           edge="start"
-//           className={classes.menuButton}
-//           color="inherit"
-//           aria-label="menu"
-//         >
-//           <MenuIcon />
-//         </IconButton>
-//         <img
-//           src={gcclogo}
-//           alt={gcclogo}
-//           className="home__img"
-//           style={{ width: "205.516px", height: "85px" }}
-//         />
-//         <Button color="inherit">About</Button>
-//         <Button color="inherit">Family Groups</Button>
-//         <Button color="inherit">Ministries</Button>
-//         <Button color="inherit">Giving</Button>
-//         <Button color="inherit">Multimedia</Button>
-//         <Button color="inherit">Events</Button>
-//       </Toolbar>
-//     </AppBar>
-//   );
-// }
